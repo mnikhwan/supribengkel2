3d13fd50681a83ea5b0127f0983b34c7febb9288
Muhammad Nurul Ikhwan <72797830+mnikhwan@users.noreply.github.com>
2025-04-17 16:11:51 +0700

    Update submitservice.js

--- src/js/submitservice.js
+++ src/js/submitservice.js
@@ -20,7 +20,7 @@ function submitService() {
         // alert('Please select at least one service.');
         const toast = document.createElement("div");
         toast.innerHTML = `
-        <div id="toast-danger servicekosong" class="z-50 fixed bottom-5 left-5 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-red-50 rounded-lg shadow-sm " role="alert">
+        <div id="toast-danger servicekosong" class="z-50 fixed bottom-5 left-5 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-red-50 rounded-lg shadow-sm animate-pulse " role="alert">
         <div class="inline-flex items-center justify-center shrink-0 w-8 h-8 text-red-500 bg-red-200 rounded-lg ">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
